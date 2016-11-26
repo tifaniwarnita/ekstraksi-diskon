ca4d51f0c3c49414939e86ffcd10766b6adea808
pipinkurniawati <13513089@std.stei.itb.ac.id>
2016-11-26 07:23:16 +0700

    add project

---
+++ EkstraksiDiskon/ekstraksi-diskon/src/EkstraksiDiskon.java
@@ -0,0 +1,43 @@
+import learning.DiskonClassifier;
+import learning.PreProcess;
+import twitter4j.Status;
+import util.Scrapper;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Tifani on 11/24/2016.
+ */
+public class EkstraksiDiskon {
+
+    public static void preprocessData() {
+        PreProcess.processTSV("data/diskon.tsv", PreProcess.TYPE_KLASIFIKASI_DISKON);
+        PreProcess.saveArff("data/processed/diskon/", "data/processed/diskon.arff");
+
+        PreProcess.readDictionary();
+        PreProcess.processTSV("data/kategori.tsv", PreProcess.TYPE_KATEGORI);
+        PreProcess.saveArff("data/processed/kategori/", "data/processed/kategori.arff");
+    }
+
+    public static void main(String[] args) throws Exception {
+        //PreProcess.readDictionary();
+        PreProcess.processTSV("data/kategori.tsv", PreProcess.TYPE_KATEGORI);
+//        PreProcess.saveArff("data/processed/kategori/", "data/processed/kategori.arff");
+
+//         preprocessData();
+//        Scrapper.config();
+//        ArrayList<Status> tweets = Scrapper.getTweets("diskon", 100);
+//        int i = 1;
+//        for(Status tweet : tweets) {
+//            boolean isDiskon = DiskonClassifier.classifyTweet(DiskonClassifier.PATH_MODEL_KLASIFIKASI, tweet);
+//            System.out.print("Tweet " + i + ": ");
+//            if (isDiskon) {
+//                 System.out.println("[DISKON]");
+//            } else {
+//                 System.out.println("[BUKAN DISKON] ");
+//            }
+//            System.out.println("@" + tweet.getUser().getScreenName() + ": " + tweet.getText());
+//            i++;
+//        }
+    }
+}
